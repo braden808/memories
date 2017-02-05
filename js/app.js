3832bf998c4ae7c256d8bcc715c0c066e6875e4a
Braden Amorozo <braden808@gmail.com>
2017-02-04 20:51:14 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,87 @@
+function shuffle(){
+  var order = []
+
+  for (var i = 8; i > 0; i--) {
+    order.push(i); 
+  }
+  var reorder = []
+  for (var i = 8; i > 0; i--){
+    ra = order.splice(Math.floor(Math.random() * i), 1)[0];
+    reorder.push(ra);
+    console.log(reorder)
+  }
+  
+  document.getElementById("first").style.order = reorder[0];
+  document.getElementById("second").style.order = reorder[1];
+  document.getElementById("third").style.order = reorder[2];
+  document.getElementById("fourth").style.order = reorder[3];
+  document.getElementById("fifth").style.order = reorder[4];
+  document.getElementById("sixth").style.order = reorder[5];
+  document.getElementById("seventh").style.order = reorder[6];
+  document.getElementById("eighth").style.order = reorder[7];
+
+}
+window.onload = shuffle;
+
+var firstFlip = 0;
+var secondFlip = 0;
+var flipCounter = 0;
+
+function changea(){
+  document.getElementById("f1").style.transform = "rotateY(0deg)";
+  document.getElementById("b1").style.transform= "rotateY(-180deg)";
+  firstFlip = 1;
+  flipCounter++;
+}
+
+function changeb(){
+  document.getElementById("f2").style.transform = "rotateY(0)";
+  document.getElementById("b2").style.transform= "rotateY(-180deg)";
+  firstFlip = 2;
+  flipCounter++;
+}
+
+function changec(){
+  document.getElementById("f3").style.transform = "rotateY(0deg)";
+  document.getElementById("b3").style.transform= "rotateY(-180deg)";
+  firstFlip = 3;
+  flipCounter++;
+}
+
+function changed(){
+  document.getElementById("f4").style.transform = "rotateY(0deg)";
+  document.getElementById("b4").style.transform= "rotateY(-180deg)";
+  firstFlip = 4;
+  flipCounter++;
+}
+
+function changee(){
+  document.getElementById("f5").style.transform = "rotateY(0deg)";
+  document.getElementById("b5").style.transform= "rotateY(-180deg)";
+  secondFlip = 1;
+  flipCounter++;
+}
+
+function changef(){
+  document.getElementById("f6").style.transform = "rotateY(0deg)";
+  document.getElementById("b6").style.transform= "rotateY(-180deg)";
+  secondFlip = 2;
+  flipCounter++;
+}
+
+function changeg(){
+  document.getElementById("f7").style.transform = "rotateY(0deg)";
+  document.getElementById("b7").style.transform= "rotateY(-180deg)";
+  secondFlip = 3;
+  flipCounter++;
+}
+
+function changeh(){
+  document.getElementById("f8").style.transform = "rotateY(0deg)";
+  document.getElementById("b8").style.transform= "rotateY(-180deg)";
+  secondFlip = 4;
+  flipCounter++;
+
 }
 
 function check(){
@@ -51,4 +135,3 @@ function check(){
       }
   },1000);
 }
-
