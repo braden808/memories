3004b828946d68db5282233e9ed8e70e01f4e493
Braden Amorozo <braden808@gmail.com>
2017-02-08 09:17:51 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -88,50 +88,64 @@ function check(){
   console.log(firstFlip);
   console.log(secondFlip);
   console.log(flipCounter);
+  console.log(cardcount);
   console.log("--------");
 
-  setTimeout(function(){
-    if(firstFlip == secondFlip){
-      if(firstFlip == 1){
-        document.getElementById("first").style.display = "none";
-        document.getElementById("fifth").style.display = "none";
-      }
-      if(firstFlip == 2){
-        document.getElementById("second").style.display = "none";
-        document.getElementById("sixth").style.display = "none";
-      }
-      if(firstFlip == 3){
-        document.getElementById("third").style.display = "none";
-        document.getElementById("seventh").style.display = "none";
-      }
-      if(firstFlip == 4){
-        document.getElementById("fourth").style.display = "none";
-        document.getElementById("eighth").style.display = "none";
-      }
+setTimeout(function(){
+  if(firstFlip == secondFlip){
+    if(firstFlip == 1){
+      document.getElementById("first").style.display = "none";
+      document.getElementById("fifth").style.display = "none";
+      cardcount++;
+    }
+    if(firstFlip == 2){
+      document.getElementById("second").style.display = "none";
+      document.getElementById("sixth").style.display = "none";
+      cardcount++;
+    }
+    if(firstFlip == 3){
+      document.getElementById("third").style.display = "none";
+      document.getElementById("seventh").style.display = "none";
+      cardcount++;
+    }
+    if(firstFlip == 4){
+      document.getElementById("fourth").style.display = "none";
+      document.getElementById("eighth").style.display = "none";
+      cardcount++;
+    }
 
 
+    flipCounter = 0;
+    firstFlip = 0;
+    secondFlip = 0; 
+  }
+}, 1500);
+
+setTimeout(
+  function delay(){
+    if(flipCounter == 2 && firstFlip != secondFlip){
       flipCounter = 0;
       firstFlip = 0;
-      secondFlip = 0; 
-    }
-  }, 1500);
-
-  setTimeout(
-    function delay(){
-      if(flipCounter == 2 && firstFlip != secondFlip){
-        flipCounter = 0;
-        firstFlip = 0;
-        secondFlip = 0;
+      secondFlip = 0;
+      
+      var idFrontNames = ["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8"];
+      var idBackNames = ["b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8"];
+      for(var z = idFrontNames.length; z > 0; z--){
+        frontId = idFrontNames.splice(0, 1)[0];
+        backId = idBackNames.splice(0, 1)[0];
         
-        var idFrontNames = ["f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8"];
-        var idBackNames = ["b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8"];
-        for(var z = idFrontNames.length; z > 0; z--){
-          frontId = idFrontNames.splice(0, 1)[0];
-          backId = idBackNames.splice(0, 1)[0];
-          
-          document.getElementById(frontId).style.transform = "rotateY(-180deg)";
-          document.getElementById(backId).style.transform = "rotateY(0deg)";    
-        }
+        document.getElementById(frontId).style.transform = "rotateY(-180deg)";
+        document.getElementById(backId).style.transform = "rotateY(0deg)";    
       }
+    }
   },1000);
+}
+
+var cardcount = 0;
+
+function win() {
+  var victory = document.getElementById("row1");
+  if(cardcount == 3){
+        victory.style.backgroundColor = "lightblue";
+  }
 }
